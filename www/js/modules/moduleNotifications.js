8f18a65557ef051aaea0bb37231531ff603a84ae
Josekaris038 <62815504+Josekaris038@users.noreply.github.com>
2026-05-30 11:11:07 +0300

    newmoduleNotifications.js

--- www/js/modules/moduleNotifications.js
+++ www/js/modules/moduleNotifications.js
@@ -1,7 +1,1 @@
-ModuleLoader.register("notifications", function () {
-  moduleContainer.innerHTML = "<h2 class='module-title'>Notifications</h2>";
 
-return function cleanup() {
-  window.removeEventListener("inventoryUpdated", render);
-};
-});
